25a487b4ab53696eb06daa926a508b7064f7d7d1
Arseny20 <56798265+Arseny20@users.noreply.github.com>
2025-11-02 20:12:36 +0300

    Update saving_model.py

--- saving_model.py
+++ saving_model.py
@@ -4,5 +4,6 @@ from transformers import AutoTokenizer, AutoModel
 tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
 model = AutoModel.from_pretrained('intfloat/multilingual-e5-large')
 
-model.save_pretrained("/home/sofia/Desktop/Rag_statya/models/multilingual-e5-large")
-tokenizer.save_pretrained("/home/sofia/Desktop/Rag_statya/models/tokenizer/multilingual-e5-large")
+model.save_pretrained("./model/multilingual-e5-large")
+
+tokenizer.save_pretrained("./models/tokenizer/multilingual-e5-large")
